0459fbf0367ef195066a4438633bff1df3171f41
Issam Elmakhfi <ielmakhf@e1r4p10.1337.ma>
2022-12-27 20:08:00 +0100

    fix ex02

--- Module01/ex02/main.cpp
+++ Module01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ielmakhf <ielmakhf@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/24 16:57:06 by ielmakhf          #+#    #+#             */
-/*   Updated: 2022/12/24 17:17:45 by ielmakhf         ###   ########.fr       */
+/*   Updated: 2022/12/27 20:06:45 by ielmakhf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int main(void)
     std::string str = "HI THIS IS BRAIN";
     std::string *stringPTR = &str;
     std::string &stringREF = str;
-    std::cout << "The memory address of \33[0;32mstr\33[0m          : " <<  &str << std::endl;
+    std::cout << "The memory address of \33[0;32mstr\33[0m        : " <<  &str << std::endl;
     std::cout << "The memory address of \33[0;32mstringPTR\33[0m  : " << &stringPTR << std::endl;
     std::cout << "The memory address of \33[0;32mstringREF\33[0m  : " << &stringREF << std::endl;
     
