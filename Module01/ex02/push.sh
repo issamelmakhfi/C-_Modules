80aa6e5f656ff5935af73f40bba3d6581f13ea20
Issam Elmakhfi <ielmakhf@e1r4p10.1337.ma>
2022-12-27 20:20:16 +0100

    ex02 is done

--- Module01/ex02/push.sh
+++ Module01/ex02/push.sh
@@ -4,12 +4,12 @@
 
 git add .
 
+git status
+
 echo -e "Please enter the Commit : "
 read commit
 echo "Your Commit is \"$commit\""
 
 git commit -m "$commit"
 
-@git push
-
-echo "\33[0;32mDone\33[0m"
+git push
