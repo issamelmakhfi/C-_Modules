688abc90e79edb9b2651f6d997c0f1976edc5a9a
Issam Elmakhfi <ielmakhf@e1r6p10.1337.ma>
2022-12-30 17:14:05 +0100

    start Module02

---
+++ Module02/ex01/fixed.hpp
@@ -0,0 +1,26 @@
+#ifndef _fixed_HPP_
+#define _fixed_HPP_
+
+#include <iostream>
+
+// ******************************************************** //
+//                         CLASSES                         //
+// ****************************************************** //
+
+class	fixed
+{
+	public	:
+		fixed ();
+		fixed (fixed const &obj);
+		~fixed ();
+		fixed &operator= (const fixed &obj);
+
+	private	:
+		//	DataType	attributes.
+};
+
+// ******************************************************** //
+//                        FUNCTIONS                        //
+// ****************************************************** //
+
+#endif
---
+++ Module02/ex01/main.cpp
@@ -0,0 +1,10 @@
+#include "fixed.hpp"
+
+int main()
+{
+	//	You Code
+
+	std::cout << "******	MAR_BEN CREATION √	******" << std::endl;
+
+	return (0);
+}
