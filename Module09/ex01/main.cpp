2ca6394e9174677d0d656de3d227047571898d70
akasec staff 1 <akasecstaff1@akasecs-iMac.local>
2023-03-18 04:20:07 +0100

    DONE : ex01

--- Module09/ex01/main.cpp
+++ Module09/ex01/main.cpp
@@ -33,7 +33,7 @@ int main(int ac, char **av)
 {
 	int i = 0;
 	int	top1;
-	int top2;
+	int top2 = 0;
 	std::stack<int> stack;
 
 	if (ac != 2)
@@ -42,24 +42,31 @@ int main(int ac, char **av)
 	while (av[1][i])
 	{
 		if (isdigit(av[1][i]))
-			stack.push(av[1][i]);
-		else
+			stack.push(av[1][i] - 48);
+		if (!isoperator(av[1][i]) && av[1][i] != ' ')
 		{
 			top1 = stack.top();
 			stack.pop();
-			top2 = stack.top();
+			if (!stack.empty())
+			{
+				top2 = stack.top();
+				stack.pop();
+			}
 			if (av[1][i] == '+')
-				stack.push(top1 + top2);
-
+				stack.push(top2 + top1);
+			if (av[1][i] == '-')
+				stack.push(top2 - top1);
+			if (av[1][i] == '*')
+				stack.push(top2 * top1);
+			if (av[1][i] == '/')
+				stack.push(top2 / top1);					
 		}
 		i++;
 	}
 	while (!stack.empty())
 	{
-		std::cout << (char)stack.top() << " ";
+		std::cout << stack.top();
 		stack.pop();
 	}
-
-
 	return (0);
 }
