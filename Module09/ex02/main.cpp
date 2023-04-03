8af2d7c88d9aaeaedfbaf1c549dba1ab23a8fa14
Issam Elmakhfi <ielmakhf@e1r2p11.1337.ma>
2023-04-03 01:53:24 +0000

    Need implement merge algo

--- Module09/ex02/main.cpp
+++ Module09/ex02/main.cpp
@@ -19,6 +19,44 @@ void    readArgs(char *av[])
 }
 
 template <class T>
+void    insertionSort(T *A, int lidx, int ridx)
+{
+    int j;
+
+    for (int i = lidx; i < ridx; i++)
+    {
+        j = i + 1;
+        while (j > lidx && (*A)[j - 1] > (*A)[j])
+        {
+            std::swap((*A)[j], (*A)[j - 1]);
+            j--;
+        }
+    }
+}
+
+template <class T>
+void    merge(T *A, int ldix, int rdix)
+{
+
+}
+
+template <class T>
+void    mergeSort(T *A, int ldix, int ridx)
+{
+    int k = 2;
+    int mid;
+
+    if (rdix - ldix > k)
+    {
+        mid = (rdix + lidx) / 2;
+        mergeSort(A, ldix, r);
+        mergeSort(A, mid + 1, rdix);
+    }
+    else
+        insertionSort(A, ldix, rdix);
+}
+
+template <class T>
 void    algorithm(char *av[], int ac)
 {
     T v;
@@ -27,6 +65,7 @@ void    algorithm(char *av[], int ac)
     {
         v.push_back(atoi(av[i]));
     }
+    insertionSort<T>(&v, 0, v.size());
     for (size_t i = 0; i < v.size(); i++)
     {
         std::cout << v[i] << std::endl;
